f01c00576f49295859ddfa3ebd4e5ac970194978
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-18 17:11:45 +0200

    completed task for common divisor

---
+++ comdivisor/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// GreatestDivisor returns the greatest divisor between two numbers
+func GreatestDivisor(a, b int) int {
+	if a == 0 {
+		return b
+	}
+
+	for b != 0 {
+		if a > b {
+			a = a - b
+			continue
+		}
+		b = b - a
+	}
+	return a
+}
+
+func main() {
+	fmt.Print(GreatestDivisor(21, 9))
+}
