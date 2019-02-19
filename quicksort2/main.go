40fc67c541b123e16160efa38e2881237c3c51f9
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-19 14:58:30 +0200

    completed quicksort task with another implementation FINAL

---
+++ quicksort2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// QuickSort arranges the numbers in a slice from the lowest to the highest
+func QuickSort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+	biggestNum := a[len(a)/2]
+
+	var rightSlice []int
+	for _, v := range a {
+		if v > biggestNum {
+			biggestNum = v
+		}
+	}
+
+	for _, v := range a {
+		if v < biggestNum {
+			rightSlice = append(rightSlice, v)
+		}
+	}
+	a = append(append([]int{}, QuickSort(rightSlice)...))
+	a = append(a, biggestNum)
+	return a
+}
+
+func main() {
+	slice := []int{9, 3, -4, 6, -5, 4}
+	s := QuickSort(slice)
+	fmt.Print(s)
+}
