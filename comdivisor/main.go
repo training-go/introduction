a84de7d3cadbcaaa6ad5d31f88c922f40e83dc00
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-21 11:48:24 +0200

    removed the comment code

--- comdivisor/main.go
+++ comdivisor/main.go
@@ -20,28 +20,3 @@ func greatestDivisor(a, b int) int {
 func main() {
 	fmt.Print(greatestDivisor(63, 9))
 }
-
-
-
-//algorithm quicksort(A, lo, hi) is
-//if lo < hi then
-//p := partition(A, lo, hi)
-//quicksort(A, lo, p - 1)
-//quicksort(A, p + 1, hi)
-//
-//algorithm partition(A, lo, hi) is
-//pivot := A[hi]
-//i := lo
-//for j := lo to hi - 1 do
-//if A[j] < pivot then
-//swap A[i] with A[j]
-//i := i + 1
-//swap A[i] with A[hi]
-//return i
-
-
-
-//I am adding this comment to test git commit
-
-
-//adding this comment to fix the conflict
