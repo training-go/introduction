73ac8c805348cf0d14ced1fccd3d64458746aa59
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-21 18:02:41 +0200

    completed the quick sort task with 2 arrays

--- quicksort3/main.go
+++ quicksort3/main.go
@@ -5,22 +5,20 @@ import "fmt"
 //Arranges the numbers in a slice from the lowest to the highest
 func QuickSort(a []int) []int {
 	if len(a) < 2 { return a }
-	pivot := a[len(a)/2]
+	pivot := a[len(a)-1]
 
 	var rightSlice []int
 	var leftSlice []int
-	for _, v := range a {
-		if v < pivot {
-			leftSlice = append(leftSlice,v)
-		} else if v >= pivot{
-			rightSlice = append(rightSlice,v)
+	for i:=0;i<len(a)-1;i++{
+		if a[i] < pivot {
+			leftSlice = append(leftSlice,a[i])
+			continue
 		}
+		rightSlice = append(rightSlice,a[i])
 	}
-	//leftSlice = append(leftSlice,pivot)
-
-	a = append(append([]int{}, QuickSort(leftSlice)...))
-	a = append(append([]int{}, QuickSort(rightSlice)...))
-
+	a = QuickSort(leftSlice)
+	a = append(a,pivot)
+	a = append(a, QuickSort(rightSlice)...)
 	return a
 }
 
@@ -29,8 +27,7 @@ func printArray(a []int) {
 }
 
 func main(){
-	slice := []int{9, 3, -4, 6, -5, 4}
-
+	slice := []int{91, -3, -4, 6, -5, 4,4,91,4}
 	s :=QuickSort(slice)
 	printArray(s)
 }
