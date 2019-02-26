641d830d214ac9c5bd564cd2b7fb586466e3c98d
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-26 14:22:58 +0200

    fixed the comment and removed PrintSlice function

--- quicksort3/main.go
+++ quicksort3/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//Arranges the numbers in a slice from the lowest to the highest
+// QuickSort arranges the numbers in a slice from the lowest to the highest using two slices
 func QuickSort(a []int) []int {
 	if len(a) < 2 {
 		return a
@@ -24,13 +24,8 @@ func QuickSort(a []int) []int {
 	return a
 }
 
-//Prints the slice
-func PrintArray(a []int) {
-	fmt.Println(a)
-}
-
 func main() {
 	slice := []int{91, -3, -4, 6, -5, 4, 4, 91, 4}
 	s := QuickSort(slice)
-	PrintArray(s)
+	fmt.Print(s)
 }
