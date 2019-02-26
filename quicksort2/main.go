87e6d9a8f5d418bf2169611072227a5465b2502a
Nikola Hristov <nikolahr0707@icloud.com>
2019-02-26 14:17:07 +0200

    fixed the message and removed PrintArray

--- quicksort2/main.go
+++ quicksort2/main.go
@@ -1,10 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-//Arranges the numbers in a slice from the lowest to the highest
+// QuickSort arranges the numbers in a slice from the lowest to the highest
 func QuickSort(a []int) []int {
 	if len(a) < 2 {
 		return a
@@ -28,14 +26,8 @@ func QuickSort(a []int) []int {
 	return a
 }
 
-//Prints slice
-func PrintArray(a []int) {
-	fmt.Println(a)
-}
-
 func main() {
 	slice := []int{9, 3, -4, 6, -5, 4}
-
 	s := QuickSort(slice)
-	PrintArray(s)
+	fmt.Print(s)
 }
