2d4d88608a32d5a30bc298929f7a0089b9dbe866
Nikola Hristov <nikolahr0707@icloud.com>
2019-04-15 15:38:55 +0300

    formated code

--- quicksort2/main_test.go
+++ quicksort2/main_test.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestQuickSort(t *testing.T) {
-	s:=QuickSort([]int{9, 3, -4, 6, -5, 3})
-	result := []int{-5,-4, 3, 3, 6, 9}
-	if reflect.DeepEqual(s, result)!=true{
-		t.Errorf("Quicksort was incorrect, want %d, got %d",result,s)
+	s := QuickSort([]int{9, 3, -4, 6, -5, 3})
+	result := []int{-5, -4, 3, 3, 6, 9}
+	if reflect.DeepEqual(s, result) != true {
+		t.Errorf("Quicksort was incorrect, want %d, got %d", result, s)
 	}
 }
--- sliceprog/main_test.go
+++ sliceprog/main_test.go
@@ -3,15 +3,15 @@ package main
 import "testing"
 
 func TestGetMinElement(t *testing.T) {
-	min:=GetMinElement([]int{4, 3, 6, -7, 2, 8})
-	if min!= -7{
-		t.Errorf("Min element was wrong, got %d, want %d",min,-7)
+	min := GetMinElement([]int{4, 3, 6, -7, 2, 8})
+	if min != -7 {
+		t.Errorf("Min element was wrong, got %d, want %d", min, -7)
 	}
 }
 
 func TestGetSum(t *testing.T) {
-	sum:=GetSum([]int{4, 3, 6, -7, 2, 8})
-	if sum!= 16{
-		t.Errorf("Sum was incorrect, got %d, want %d",sum,16)
+	sum := GetSum([]int{4, 3, 6, -7, 2, 8})
+	if sum != 16 {
+		t.Errorf("Sum was incorrect, got %d, want %d", sum, 16)
 	}
 }
